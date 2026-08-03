18e386f4509d0b0f26a2bb79f4a45b9dd422bc56
Wataru TOYA <watrty@gmail.com>
2026-08-03 17:38:56 +0900

    brefore_action で customer_id を属性として付与する

--- tracing/demo/sample-app/ruby/app/controllers/application_controller.rb
+++ tracing/demo/sample-app/ruby/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :set_customer_id
+
+  private
+
+  def set_customer_id
+    current_span = OpenTelemetry::Trace.current_span
+    current_span.set_attribute('customer.id', params[:customer_id]) if current_span.recording?
+  end
 end
